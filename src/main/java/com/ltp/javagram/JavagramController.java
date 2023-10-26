6ce4b8c2592d6313ed8024089189de71a7e41057
Matthew Hunter Steffner <huntersteffner@gmail.com>
2023-10-26 14:01:08 -0400

    Added fields

--- src/main/java/com/ltp/javagram/JavagramController.java
+++ src/main/java/com/ltp/javagram/JavagramController.java
@@ -3,18 +3,27 @@ package com.ltp.javagram;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class JavagramController {
 
     @GetMapping("/") 
     public String getForm(Model model) {
+        model.addAttribute("user", new User());
         return "sign-up";
+    }
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(User user, RedirectAttributes redirectAttributes) {
+        return "redirect:/result";
     }
 
     @GetMapping("/result")
     public String getResult() {
         return "result";
     }
+
     
 }
